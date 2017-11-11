a5b9858551bf2a30a317ac0f62775bf78ee71be4
Francesca4242 <32270681+Francesca4242@users.noreply.github.com>
2017-11-11 18:10:19 +0000

    loop to assign emails to names

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
  
 
     $("#add").click(function() {
-  $(".form").append('<div class="entry"><label for="name">Name</label> <input type="text" id="name" name="user_name"> <label for="email">Email</label> <input type="text" id="email" name="address"></div>')
+  $(".form").append('<div class="entry"><label for="name">Name</label> <input type="text" id="name" class="user_name"> <label for="email">Email</label> <input type="text" id="email" class="address"></div>')
 });
 
 
@@ -15,20 +15,38 @@ $( document ).ready(function() {
 });
 
    $("#send").click(function() {
-   	// counts number of text boxes
-   	var numItem= $('.entry').length;
-   	alert(numItem);
+    // counts number of text boxes
+    var numItem= $('.entry').length;
 
-   	//creates array of numbers 
-	var number=Array.apply(null, {length: numItem}).map(Number.call, Number);
-	alert(number);
-	var points = number;
-	//randomises array of numbers
-	points.sort(function(a, b){return 0.5 - Math.random()});
-	alert(points);
-	
-	var x =points[0];
-	var numItem=$('.email')[x];
+    //creates array of numbers 
+    var sequence=Array.apply(null, {length: numItem}).map(Number.call, Number);
+
+    //randomises array of numbers
+    sequence.sort(function(a, b){return 0.5 - Math.random()});
+
+
+    //loop to assign emails to names
+
+    for (var i=0; i<numItem; i=i+1){
+         
+        var an_email=$('.address')[sequence[i]];
+
+        var y=i+1;
+
+        if (y==numItem) {
+            y=0;
+        }
+
+        
+        var a_name=$('.user_name')[sequence[y]];
+        
+
+        //parameters: service_id, template_id, template_parameters
+        emailjs.send("yourhelpelf_gmail_com","secret_santa",{name: a_name.value, email: an_email.value});
+
+    }
+
+
 
 
    });
